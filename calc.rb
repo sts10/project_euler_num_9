20fad86efbbd51b3300349c834f4dec54810ae82
sts10 <sschlinkert@gmail.com>
2014-04-30 17:02:37 -0400

    cleaned up, but could be smoother

--- calc.rb
+++ calc.rb
@@ -1,25 +1,20 @@
-def triplet?(a, b, c)
-  (a**2 + b**2 == c**2) && (a < b) && (b < c)
-end 
-
-def omni_triplet?(a,b,c)
-  (a**2 + b**2 == c**2) || (c**2 + b**2 == a**2) || (a**2 + c**2 == b**2)
-end 
-
 
 def sum_one_thousand?(a,b,c)
   a + b + c == 1000
 end 
 
+def makes_triplet?(a,b)
+  c = (a**2 + b**2)**0.5
+  c % 1 == 0 
+end
+
 def works?(a, b)
   c = (a**2 + b**2)**0.5
-  c % 1 == 0 && sum_one_thousand?(a, b, c)
+  makes_triplet?(a,b) && sum_one_thousand?(a, b, c)
 end 
 
 def the_triplet
   a = 2 
-  
-
   while (a < 1000)
     b = 2
     while (b < 1000)
--- spec/special_pythag_triplet_spec.rb
+++ spec/special_pythag_triplet_spec.rb
@@ -3,16 +3,13 @@ require_relative '../calc.rb'
 
 describe 'can recognize triplets' do 
   it 'knows that 3 4 5 is a triplet' do 
-    expect(omni_triplet?(3,4,5)).to be(true)
+    expect(makes_triplet?(3,4)).to be(true)
   end 
 
   it 'knows that 2 4 5 is not a triplet' do 
-    expect(omni_triplet?(2,4,5)).to be(false)
+    expect(makes_triplet?(2,4)).to be(false)
   end
 
-  it 'knows that 3 5 4 is a triplet' do 
-    expect(omni_triplet?(3,5,4)).to be(true)
-  end
 end
 
 describe "Find the triplet" do
